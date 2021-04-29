919870b01ff9a9a1d0fee18867fcd1f74c26e584
pbullor <pablobullor@gmail.com>
2021-04-29 12:07:02 +0000

    subo ejemplo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,21 +5,26 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	//const [variable,funcion(que modifica mi variable)] = useState(//estado inicial/defecto (si deseo/a))
 	const [color, setColor] = useState("");
+
 	return (
-		<div className="contaniner">
+		<div className="container">
+			<h1>El color seleccionado es: {color}</h1>
 			<button
-				className={color == "red" ? "red" : ""}
+				className={color == "red" ? color : ""}
 				onClick={() => setColor("red")}>
 				Red
 			</button>
 			<button
-				className={color == "yellow" ? "yellow" : ""}
+				className={
+					color == "yellow" ? color : color == "red" ? "texto" : ""
+				}
 				onClick={() => setColor("yellow")}>
 				Yellow
 			</button>
 			<button
-				className={color == "green" ? "green" : ""}
+				className={color == "green" ? color : ""}
 				onClick={() => setColor("green")}>
 				Green
 			</button>
